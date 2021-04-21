e0d8b6c96d7c3d57b420a4b4f4942c9545c77124
飯盛正慶 <masamichhhi@gmail.com>
2021-04-21 11:16:50 +0900

    add service implement

--- article/service/service.go
+++ article/service/service.go
@@ -12,7 +12,7 @@ type Service interface {
 	ReadArticle(ctx context.Context, req *pb.ReadArticleRequest) (*pb.ReadArticleResponse, error)
 	UpdateArticle(ctx context.Context, req *pb.UpdateArticleRequest) (*pb.UpdateArticleResponse, error)
 	DeleteArticle(ctx context.Context, req *pb.DeleteArticleRequest) (*pb.DeleteArticleResponse, error)
-	ListArticle(req *pb.ListArticleRequest, stream pb.ArticleService_ListArticleClient) error
+	ListArticle(req *pb.ListArticleRequest, stream pb.ArticleService_ListArticleServer) error
 }
 
 type service struct {
@@ -24,21 +24,88 @@ func NewService(r repository.Repository) Service {
 }
 
 func (s *service) CreateArticle(ctx context.Context, req *pb.CreateArticleRequest) (*pb.CreateArticleResponse, error) {
+	input := req.GetArticleInput()
 
+	id, err := s.repository.InsertArticle(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreateArticleResponse{
+		Article: &pb.Article{
+			Id:      id,
+			Author:  input.Author,
+			Title:   input.Title,
+			Content: input.Content,
+		},
+	}, nil
 }
 
 func (s *service) ReadArticle(ctx context.Context, req *pb.ReadArticleRequest) (*pb.ReadArticleResponse, error) {
 	// 記事のREAD処理
+	id := req.GetId()
+
+	a, err := s.repository.SelectArticleByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ReadArticleResponse{
+		Article: &pb.Article{
+			Id:      id,
+			Author:  a.Author,
+			Title:   a.Title,
+			Content: a.Content,
+		},
+	}, nil
 }
 
 func (s *service) UpdateArticle(ctx context.Context, req *pb.UpdateArticleRequest) (*pb.UpdateArticleResponse, error) {
-	// 記事のUPDATE処理
+	id := req.GetId()
+
+	input := req.GetArticleInput()
+
+	if err := s.repository.UpdateArticle(ctx, id, input); err != nil {
+		return nil, err
+	}
+
+	return &pb.UpdateArticleResponse{
+		Article: &pb.Article{
+			Id:      id,
+			Author:  input.Author,
+			Title:   input.Title,
+			Content: input.Content,
+		},
+	}, nil
 }
 
 func (s *service) DeleteArticle(ctx context.Context, req *pb.DeleteArticleRequest) (*pb.DeleteArticleResponse, error) {
-	// 記事のDELETE処理
+	id := req.GetId()
+
+	if err := s.repository.DeleteArticle(ctx, id); err != nil {
+		return nil, err
+	}
+
+	return &pb.DeleteArticleResponse{Id: id}, nil
 }
 
 func (s *service) ListArticle(req *pb.ListArticleRequest, stream pb.ArticleService_ListArticleServer) error {
-	// 記事の全取得処理
+	rows, err := s.repository.SelectAllArticles()
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var a pb.Article
+		err := rows.Scan(&a.Id, &a.Author, &a.Title, &a.Content)
+
+		if err != nil {
+			return err
+		}
+		stream.Send(&pb.ListArticleResponse{Article: &a})
+	}
+
+	return nil
 }
